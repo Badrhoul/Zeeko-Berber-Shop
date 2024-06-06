5d712b1b1568e5e1e4ce8d2a959040f41d29a26b
Badreddine El Rhoul <badrhoul@gmail.com>
2024-06-06 16:08:27 +0200

    Set up route for homepage

--- client/client/src/main.jsx
+++ client/client/src/main.jsx
@@ -4,10 +4,15 @@ import {createBrowserRouter,RouterProvider} from 'react-router-dom'
 import App from './App.jsx'
 import './index.css'
 
-// const router = createBrowserRouter([]);
+const router = createBrowserRouter([
+  {
+    path:'/',
+    element:<App/>
+  }
+]);
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router = {router}/>
   </React.StrictMode>,
 )
